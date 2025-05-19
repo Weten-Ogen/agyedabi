630cc4bd9019c5485ffefb86186fa784184a82df
Weten-Ogen <alphaoware@gmail.com>
2025-05-19 12:09:13 +0000

    fixed the font-poppins

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import { AdminContent, adminPageContent } from '../../../../content/general'
 import AdminChart from './adminchart'
+import { auth } from '@/lib/auth'
+import { redirect } from 'next/navigation'
 
-export default function Admin() {
+export default async function Admin() {
+  const session = await auth()
+  if(session?.user.role?.toLowerCase() !== "admin") {
+    redirect('/home')
+  }
   return (
     <section className='mt-20'>
       <div className='flex gap-8 '>
