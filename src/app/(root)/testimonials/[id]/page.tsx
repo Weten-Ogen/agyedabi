5dae501dc33a74746e6b6958a1a3cf5b9bdfbf15
Weten-Ogen <alphaoware@gmail.com>
2025-05-20 06:38:49 +0000

    added the git payments hero card

--- src/app/(root)/testimonials/[id]/page.tsx
+++ src/app/(root)/testimonials/[id]/page.tsx
@@ -1,32 +1,29 @@
-"use client"
-import React from 'react'
-import { AdminContent } from '../../../../../content/general'
-import Image from 'next/image'
+// src/app/(root)/testimonials/[id]/page.tsx
+import Image from 'next/image';
+import { AdminContent } from '../../../../../content/general';
 
-
-
-export default  function Page({ params}:{params: {id: string}}) {
-  const { id } =  params;
- 
-  const user = AdminContent.HeroTestimonials.find(item => item.id === id);
+export default function Page({ params }: { params: { id: string } }) {
+  const { id } = params;
+  const user = AdminContent.HeroTestimonials.find((item) => item.id === id);
 
   if (!user) {
     return <div>User not found</div>;
   }
 
   return (
-    <div className='mt-14 w-full p-4  flex flex-col gap-8 '>
-      <div className='w-[90%] max-w-full object-cover bg-acc-color p-4 mx-auto'>
-        <Image 
-        className='object-contain w-full h-[50vh]'
-        src={user.imageUrl} 
-        alt={user.name} 
-        priority={true}
-        width={500} 
-        height={400} />
+    <div className="mt-14 w-full p-4 flex flex-col gap-8">
+      <div className="w-[90%] max-w-full object-cover bg-acc-color p-4 mx-auto">
+        <Image
+          className="object-contain w-full h-[50vh]"
+          src={user.imageUrl}
+          alt={user.name}
+          priority
+          width={500}
+          height={400}
+        />
       </div>
-      <div className='max-w-2xl mx-auto  flex flex-col gap-8'>
-        <h1 className='text-smm font-bold'>{user.name}</h1>
+      <div className="max-w-2xl mx-auto flex flex-col gap-8">
+        <h1 className="text-smm font-bold">{user.name}</h1>
         <p>{user.content}</p>
       </div>
     </div>
