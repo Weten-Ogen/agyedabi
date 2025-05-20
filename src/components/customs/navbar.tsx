35248f5dbd0cc817de8360d1d87649770ff738fc
Weten-Ogen <alphaoware@gmail.com>
2025-05-20 08:08:38 +0000

    fixed errors with params

--- src/components/customs/navbar.tsx
+++ src/components/customs/navbar.tsx
@@ -21,7 +21,7 @@ const NavBar = (props:navbarprops) => {
   return (
     <div className={cn('flex items-end  px-6 md:px-8 py-2   w-full fixed inset-0 z-30 bg-white border  justify-between h-12 shadow-lg  ',props.className)}>
       <div className='flex items-center gap-10'>
-          <h4 onClick={() => router.push('/')} className=' cursor-pointer text-smm uppercase  md:p-0  font-bold'>
+          <h4 onClick={() => router.push('/')} className=' cursor-pointer text-smm uppercase  md:p-0  font-bold font-space-grotesk '>
               {AdminContent.appName}
           </h4>
           <NavBarLinks/>
