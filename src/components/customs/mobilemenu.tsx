f303bd81c44810c276788b30e43463714eb7d79d
Weten-Ogen <alphaoware@gmail.com>
2025-05-21 06:51:38 +0000

    completed the course form

--- src/components/customs/mobilemenu.tsx
+++ src/components/customs/mobilemenu.tsx
@@ -9,6 +9,7 @@ import Link from 'next/link'
 import SignOutBtn from './signoutbtn'
 import { Avatar, AvatarImage } from '../ui/avatar'
 import { useSession } from 'next-auth/react'
+import { Separator } from '../ui/separator'
 
 interface mobilemenuprops {
   className?: string
@@ -30,26 +31,27 @@ const MobileMenu = (props: mobilemenuprops) => {
         <DropdownMenuTrigger asChild>
           <Menu className='text-sec-color hover:text-acc-color' />
         </DropdownMenuTrigger>
-        <DropdownMenuContent className='relative right-8 p-2 w-full flex items-start gap-4 md:p-0'>
-          <div className='p-1'>
+        <DropdownMenuContent className='relative right-8  w-full flex items-baseline gap-0 md:p-0'>
+          <div className='pl-4'>
             {image && (
               <Avatar>
                 <AvatarImage
-                className='h-8 w-8 rounded-full bg-acc-color' 
+                className='h-8 w-12   rounded-full ' 
                 alt='User Avatar' 
                 src={image} />
               </Avatar>
             )}
           </div>
-          <div className='p-2'>
+          <div className='p-0'>
             <div className='flex flex-col gap-2 p-4'>
               {AdminContent.navbarlinks.map((item: any, i: number) => (
-                <DropdownMenuItem className='text-acc-color text-sml p-0 font-poppins' key={i}>
+                <DropdownMenuItem className='text-acc-color flex flex-col gap-1 text-sml p-0 font-poppins' key={i}>
                   <DropdownMenuLabel className='hover:bg-acc-color w-full rounded-lg hover:text-white p-2 font-poppins'>
                     <Link onClick={handleClick} className='w-full text-sml capitalize font-poppins' href={item.href}>
                       {item.label}
                     </Link>
                   </DropdownMenuLabel>
+                  <Separator/>
                 </DropdownMenuItem>
               ))}
             </div>
