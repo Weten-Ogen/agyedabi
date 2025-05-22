5d55d9c0b5b00cfc134ef4c8ecd4b2f729a0c0c0
Weten-Ogen <alphaoware@gmail.com>
2025-05-22 02:07:07 +0000

    added padding to home

--- src/app/(root)/quiz/page.tsx
+++ src/app/(root)/quiz/page.tsx
@@ -7,7 +7,7 @@ import { redirect } from 'next/navigation';
 
 export default async function page() {
   const session = await auth();
-  if(session?.user.role?.toLowerCase() === 'admin'){
+  if(session?.user.role?.toLowerCase() !== 'admin'){
     
     redirect('/home')
   }
