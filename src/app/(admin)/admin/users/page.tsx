897585c55c455b744b6fe81960abbcd96bd41a0f
Weten-Ogen <alphaoware@gmail.com>
2025-05-25 03:39:57 +0000

    the course registration

--- src/app/(admin)/admin/users/page.tsx
+++ src/app/(admin)/admin/users/page.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { PrismaStudioTable } from '../data_table'
-import { columns } from '../column'
+// import { PrismaStudioTable } from '../data_table'
+// import { columns } from '../column'
 import { users } from '../quiz'
 
 export default function page() {
   return (
     <div className='mt-20 p-8'>
-        <PrismaStudioTable
+        {/* <PrismaStudioTable
           columns={columns}
           data={users}
-        />
+        /> */}
     </div>
   )
 }
